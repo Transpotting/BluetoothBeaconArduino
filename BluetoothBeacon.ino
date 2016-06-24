04dc6ba2b5816c41b831b2c2cebfcbbe0660a980
Ivan Voras <ivoras@gmail.com>
2016-06-24 21:42:19 +0200

    Bounce strings between BT and serial

--- BluetoothBeacon.ino
+++ BluetoothBeacon.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+#define BT_NAME "BUS-1070"
+
+SoftwareSerial btModule(2,3);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  
+  Serial.begin(9600);
+  btModule.begin(9600);
+
+  delay(100);
+  btModule.print("AT+NAME" BT_NAME);
+  
+  digitalWrite(13, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (btModule.available()) {
+    String line = btModule.readStringUntil('\n');
+    Serial.print(line);
+  }
 
+  if (Serial.available()) {
+    String line = Serial.readStringUntil('\n');
+    btModule.print(line);
+  }
 }
